b1ec07dd99c91ee65077d5c19f2cc4635cf822bf
Davidyan666 <2315427303@qq.com>
2026-07-20 18:55:49 +0800

    小修复

--- boot/loader.asm
+++ boot/loader.asm
@@ -23,6 +23,19 @@ start:
     mov dl,[BOOT_DRIVE]
     int 0x13
 
+    mov ah,0x41
+    mov bx,0x55AA
+    mov dl,[BOOT_DRIVE]
+    int 0x13
+    jc .no_edd
+    cmp bx,0xAA55
+    jne .no_edd
+    test cx,1
+    jz .no_edd
+    mov byte [EDD_AVAILABLE],1
+
+.no_edd:
+
     ; 分块加载Kernel，避免BIOS单次读扇区数限制
     mov word [KERNEL_REMAINING],512
 
@@ -38,12 +51,15 @@ load_kernel:
 .use_remaining:
     mov [KERNEL_DAP_COUNT],ax
 
+    cmp byte [EDD_AVAILABLE],0
+    je load_kernel_chs
+
     mov si,KERNEL_DAP
     mov ah,0x42
     mov dl,[BOOT_DRIVE]
     int 0x13
 
-    jc disk_error
+    jc load_kernel_chs
 
     mov ax,[KERNEL_DAP_COUNT]
     sub [KERNEL_REMAINING],ax
@@ -56,6 +72,22 @@ load_kernel:
     adc word [KERNEL_DAP_LBA+2],0
 
     jmp load_kernel
+
+load_kernel_chs:
+    call init_chs_geometry
+
+.next_sector:
+    mov ax,[KERNEL_REMAINING]
+    cmp ax,0
+    je kernel_loaded
+
+    call read_kernel_sector_chs
+    jc disk_error
+
+    dec word [KERNEL_REMAINING]
+    add word [KERNEL_DAP_SEGMENT],0x20
+    inc word [KERNEL_DAP_LBA]
+    jmp .next_sector
 
 kernel_loaded:
 
@@ -89,6 +121,18 @@ enter_pm:
 
 BOOT_DRIVE:
     db 0
+
+EDD_AVAILABLE:
+    db 0
+
+CHS_INITIALIZED:
+    db 0
+
+CHS_SECTORS:
+    dw 63
+
+CHS_HEADS:
+    dw 255
 
 KERNEL_DAP:
     db 0x10
@@ -125,6 +169,58 @@ disk_error:
 print_char:
     mov ah,0x0e
     int 0x10
+    ret
+
+init_chs_geometry:
+    cmp byte [CHS_INITIALIZED],0
+    jne .done
+
+    mov byte [CHS_INITIALIZED],1
+    xor ax,ax
+    mov dl,[BOOT_DRIVE]
+    int 0x13
+
+    mov ah,0x08
+    mov dl,[BOOT_DRIVE]
+    int 0x13
+    jc .done
+
+    mov al,cl
+    and al,0x3F
+    jz .done
+    xor ah,ah
+    mov [CHS_SECTORS],ax
+
+    mov al,dh
+    inc al
+    xor ah,ah
+    mov [CHS_HEADS],ax
+
+.done:
+    ret
+
+read_kernel_sector_chs:
+    mov ax,[KERNEL_DAP_SEGMENT]
+    mov es,ax
+    xor bx,bx
+
+    mov ax,[KERNEL_DAP_LBA]
+    xor dx,dx
+    div word [CHS_SECTORS]
+    mov cl,dl
+    inc cl
+
+    xor dx,dx
+    div word [CHS_HEADS]
+    mov dh,dl
+    mov ch,al
+    shl ah,6
+    or cl,ah
+
+    mov ah,0x02
+    mov al,1
+    mov dl,[BOOT_DRIVE]
+    int 0x13
     ret
 
 
